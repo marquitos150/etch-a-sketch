c11c826d9cbde2640095851a205647910cc41ed1
Marcos Vazquez <mmvazquez364@gmail.com>
2025-09-16 15:38:02 -0400

    Created 256x256 grid (containing 16x16 squares) using flex

---
+++ grid.js
@@ -0,0 +1,12 @@
+const grid = document.querySelector(".grid");
+
+for (let i = 0; i < 16; i++) {
+    const rowGrid = document.createElement("div");
+    rowGrid.classList.add("row-grid");
+    for (let j = 0; j < 16; j++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        rowGrid.appendChild(square);
+    }
+    grid.appendChild(rowGrid);
+}
